39b92d95581cb7fce2e87b8a3e2190706d504558
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2026-09-18 09:44:40 +0300

    Added api folter with countries.json and types for countries

---
+++ my-country-app/src/types/country.ts
@@ -0,0 +1,11 @@
+export type Country = {
+  id: number;
+  name: string;
+  shortInfo: string;
+  continent: string;
+  capital: string;
+  language: string;
+  population: number;
+  totalArea: number;
+  image: string;
+};
